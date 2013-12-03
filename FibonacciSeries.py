984990ab793a53c31786b962fad6f026fe453bec
PNarode <pratiknarode143@gmail.com>
2013-12-03 23:32:12 +0530

    Update and rename FibonacciSeries to FibonacciSeries.py

--- FibonacciSeries.py
+++ FibonacciSeries.py
@@ -1,6 +1,8 @@
 # Program to find the Fibonacci Sequence with the length of series equal to that specified by the user.
 # The input to the Program is the limit of the Sequence.
 #author@Pratik Narode(pratiknarode143@gmail.com)
+
+
 def fib(a,b,n):
     if n!=0:
         print a
